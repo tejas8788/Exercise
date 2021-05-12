f703d0dcc415254f4a6175352e9f545d3fda3965
tejas8788 <71746971+tejas8788@users.noreply.github.com>
2021-05-12 11:12:43 +0530

    Add files via upload

---
+++ Django-onlineexam/onlineexam/onlineexam/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from onlineexam.models import Exam
+
+def examonline(request):
+    results=Exam.objects.all()
+    return render(request,'Index.html',{"Exam":results})
